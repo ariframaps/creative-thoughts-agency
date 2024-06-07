0c94140e4a11efb0478bf563dd89334bfaabcc7e
ariframaps <ariframa@student.ub.ac.id>
2024-06-07 20:56:59 +0700

    project structure

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import Links from "./Links"
+
+export const Navbar = () => {
+    return (
+        <div>
+            <div>Logo</div>
+            <div>
+                <Links />
+            </div>
+        </div>
+    )
+}
