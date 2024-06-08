eaa3a6740d29560a48ce20151e51dbf37865f7ac
ariframaps <ariframa@student.ub.ac.id>
2024-06-08 16:47:03 +0700

    settingup prettier for tailwind

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,7 @@
 export const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
-}
+  return (
+    <div className="container flex w-3 text-wrap bg-red-300 font-serif">
+      Footer
+    </div>
+  );
+};
