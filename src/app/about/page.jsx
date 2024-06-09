5b4f9c7c93f9ee6da70bba990493341cf6cb2f2f
ariframaps <ariframa@student.ub.ac.id>
2024-06-09 19:05:35 +0700

    about page layout

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,35 @@
+import Image from "next/image";
+
 export default function AboutPage() {
-    return (
-        <div>AboutPage</div>
-    );
+  return (
+    <div className="m-auto grid gap-3 md:grid-cols-2">
+      <div>
+        <p className="mb-12 text-xl font-bold">About Agency</p>
+        <h1 className="text-5xl font-bold leading-tight lg:text-6xl">
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className="my-12">
+          We create digital ideas that are bigger, bolder, braver, and better.
+          We believe in good ideas flexibility and precission. We're world's Our
+          Special Team best consulting & finance solution provider. Wide range
+          of wev and software development services.
+        </p>
+        <div className="mb-10 flex gap-5 lg:mb-0">
+          <div>
+            <h4 className="mb-3 text-3xl font-bold">10 K+</h4>
+            <p>Year of experience</p>
+          </div>
+          <div>
+            <h4 className="mb-3 text-3xl font-bold">234 K+</h4>
+            <p>People reached</p>
+          </div>
+          <div>
+            <h4 className="mb-3 text-3xl font-bold">5 K+</h4>
+            <p>Services and plugins</p>
+          </div>
+        </div>
+      </div>
+      <Image className="bg-red-200" />
+    </div>
+  );
 }
