17e4eb672ec8fdc00fb31f86d805fd6b57c7258c
ariframaps <ariframa@student.ub.ac.id>
2024-06-09 18:29:48 +0700

    fix auth nav layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,32 @@
+import Image from "next/image";
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div className="grid grid-cols-2">
+      <div>
+        <h1 className="text-6xl font-bold leading-tight">
+          Creative
+          <br />
+          Thoughts
+          <br />
+          Agency.
+        </h1>
+        <p className="my-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
+          vero minus? Deserunt at a cumque corporis nesciunt.
+        </p>
+        <div className="flex gap-5">
+          <button className="rounded bg-white px-10 py-5 text-black">
+            Learn More
+          </button>
+          <button className="rounded bg-blue-600 px-10 py-5 text-white">
+            Contact
+          </button>
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <Image className="h-10 w-10 bg-red-200" />
+      </div>
+    </div>
+  );
 }
--- src/components/navbar/AuthNav.jsx
+++ src/components/navbar/AuthNav.jsx
@@ -6,12 +6,12 @@ const AuthNav = () => {
 
   if (isLoggedIn) {
     return (
-      <>
+      <div className="mt-3 flex flex-col gap-2 text-end md:ms-3 md:mt-0 md:flex-row">
         {isAdmin && <NavLink link={{ title: "Admin", path: "/admin" }} />}
-        <button className="mt-3 bg-white px-3 py-2 text-end text-black md:ms-3 md:mt-0">
+        <button className="bg-white px-10 py-2 text-end text-black md:px-7">
           Logout
         </button>
-      </>
+      </div>
     );
   } else {
     return <NavLink link={{ title: "Login", path: "/login" }} />;
