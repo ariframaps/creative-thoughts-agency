062b7605a5cbef6a1397c7aece4df1b4ead77ebc
ariframaps <ariframa@student.ub.ac.id>
2024-06-10 16:43:38 +0700

    fotoer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,10 @@
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mx-auto flex w-full items-center justify-between self-start px-7 py-7 text-slate-300 lg:py-10 xl:px-20 2xl:max-w-screen-2xl">
+      <p className="font-bold">Arif.dev</p>
+      <p className="text-slate-500">
+        Arif creative thoughts agency. All rights reserved.
+      </p>
+    </div>
+  );
 };
