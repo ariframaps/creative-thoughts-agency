654c1fd8f76bc9ad099bd6c36829fe50f2f4c003
ariframaps <ariframa@student.ub.ac.id>
2024-10-17 02:24:18 +0700

    fix post schema

--- src/lib/models.js
+++ src/lib/models.js
@@ -53,6 +53,11 @@ const postSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
   { timestamps: true },
 );
